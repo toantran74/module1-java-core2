9a12f98be6473b9d6758078425e38646ac086e4a
toantran74 <quoctoan090404@gmail.com>
2024-12-14 00:47:05 +0700

    Ex8

---
+++ src/ss3_java_overview/Ex8_Result.java
@@ -0,0 +1,9 @@
+package ss3_java_overview;
+
+public class Ex8_Result {
+    public static void main(String[] args) {
+        int i = 2;
+        System.out.println(i++);// i = 2
+        System.out.println(++i);// i = 4
+    }
+}
