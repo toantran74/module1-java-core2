ee88a764834aa066f4f1a343f364c334464cc99b
toantran74 <quoctoan090404@gmail.com>
2024-12-13 17:30:37 +0700

    first commit

---
+++ src/ss3_java_overview/HelloWord.java
@@ -0,0 +1,13 @@
+package ss3_java_overview;
+
+import java.time.Year;
+import java.util.Scanner;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao nam sinh cua ban: ");
+        int year = sc.nextInt();
+        System.out.println("Tuoi cua ban la: " +  (Year.now().getValue() - year));
+    }
+}
